acd9f7a5090b7d9b7c5383edf1f8764f84505928
HuaweiLap <grigorikovalenko@gmail.com>
2023-05-04 19:22:58 +0300

    m2 starting repo fixes

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # tp_alg
+# Module 2
+1. Hash
+2. Trees
+3. Haffman
+4. Graph
+5. etc
+
--- task_1.cpp
+++ task_1.cpp
@@ -1,114 +1,95 @@
-// рабочий код
 #include <iostream>
-#include <climits>
-#include <sstream>
+#include <string>
+#include <vector>
 #include <cassert>
+#include <sstream>
 
-class Soulution {
-    public:
-    size_t countBit(int input) {
-        size_t count = 0;
+void run(std::istream&, std::ostream&);
+void test();
 
-        while (input > 0) {
+enum state {
+    EMPTY,
+    FULL,
+    DELTE
+};
 
-            count += input&1;
-            input >>= 2;
-            
+
+template<class T, class H>
+class HashTable {
+public:
+    HashTable(H hasher): hasher_(hasher) {}
+    HashTable(const size_t size, H hasher): size_(size){}
+
+    ~HashTable() = default;
+
+    bool Has(const T& el);
+
+    bool Add(const T& el) {
+        for (int i = 0; i < size_; ++i) {
+            int j = h
         }
+    }
 
-        return count;
+    bool Pop(const T& el);
+private:
+    struct HashTableNode {
+        T data_;
+        state has_data {EMPTY};
+
+        HashTableNode() = default;
+        HashTableNode(const T& data, unsigned int hash): data_(data), abs_hash_(hash), has_data(FULL) {}
+    };
+
+    size_t size_;
+    size_t capacity_;
+    std::vector<HashTableNode> table_;
+    H hasher_;
+
+    double grow_default_ = 2.0;
+    double load_factor_ = 0.75;
+
+    [[nodiscard]] unsigned int hash(const unsigned int abs_hash) const {
+        return abs_hash % capacity_;
+    }
+
+    void growTable() {
+        size_t new_capacity = capacity_ * grow_default_;
+        std::vector<HashTableNode> new_tabele(new_capacity, this->size_, hasher_);
+
+
     }
 };
 
-void run(std::istream& input, std::ostream& output) {
-
-    int num; 
-    Soulution soulution;
-
-    input >> num;
-    output << soulution.countBit(num);
-
-}
-
-void testSoultion();
-
-int main() {
+int main (const int argc, const char* argv[]) {
     run(std::cin, std::cout);
+    // test();
+    return 0;
 }
 
-void testSoultion() {
-    {
-        std::stringstream input;
-        std::stringstream output;
-
-        std::cout << "Test 1 start" << std::endl;
-
-        input << "25";
-        run(input, output);
-        assert(output.str() == "2");
-
-        std::cout << "Test 1 ended" << std::endl;
-    }
-    {
-        std::stringstream input;
-        std::stringstream output;
-
-        std::cout << "Test 2 start" << std::endl;
-
-        input << "0";
-        run(input, output);
-        assert(output.str() == "0");
-
-        std::cout << "Test 2 ended" << std::endl;
-    }
-    {
-        std::stringstream input;
-        std::stringstream output;
-
-        std::cout << "Test 3 start" << std::endl;
-
-        input << "1";
-        run(input, output);
-        assert(output.str() == "1");
-
-        std::cout << "Test 3 ended" << std::endl;
-    }
-    {
-        std::stringstream input;
-        std::stringstream output;
-
-        std::cout << "Test 4 start" << std::endl;
-
-        input << "10";
-        run(input, output);
-        assert(output.str() == "0");
-
-        std::cout << "Test 4 ended" << std::endl;
-    }
-    {
-        std::stringstream input;
-        std::stringstream output;
-
-        std::cout << "Test 5 start" << std::endl;
-
-        
-        input << "100";
-        run(input, output);
-        assert(output.str() == "2");
-
-        std::cout << "Test 5 ended" << std::endl;
-    }
-    {
-        std::stringstream input;
-        std::stringstream output;
-
-        std::cout << "Test 6 start" << std::endl;
-
-        input << "4294967295";
-        run(input, output);
-        assert(output.str() == "16");
-
-        std::cout << "Test 6 ended" << std::endl;
-    }
+void run(std::istream& in, std::ostream& out) {
+    return;
 }
 
+
+void test() {
+    {
+        std::stringstream in;
+        std::stringstream out;
+
+        in << "+ hello\n + bye\n ? bye\n + bye\n - bye\n ? bye\n ? hello\n";
+
+        run(in, out);
+
+        assert(out.str() == "OK\n OK\n OK\n FAIL\n OK\n FAIL\n OK");
+    }
+    {
+        std::stringstream in;
+        std::stringstream out;
+
+        in << "+ hello\n + bye\n ? bye\n + bye\n - bye\n ? bye\n ? hello\n";
+
+        run(in, out);
+
+        assert(out.str() == "OK\n OK\n OK\n FAIL\n OK\n FAIL\n OK");
+    }
+}
